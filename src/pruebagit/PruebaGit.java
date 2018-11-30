b5e3a8e3909b9d3ae7c7cf02273bdfd293060b47
Pedro Rodirguez Lozano <r_ordep@hotmail.com>
2018-11-30 13:42:02 +0100

    Update PruebaGit.java
    Editado desde GitHub

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -17,7 +17,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola pedro con GIT");
-        System.out.println("aDios Pedro ");
+        System.out.println("aDios Pedro , editado desde GitHub");
     }
     
 }
